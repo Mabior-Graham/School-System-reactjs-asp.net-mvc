0dd085c49cc47347fbce5337e3ac61aaeb8974b5
Mabior-Graham <77841359+Mabior-Graham@users.noreply.github.com>
2022-01-14 14:18:14 +0300

    Models folder

---
+++ Students-asp/Models/Students.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Schsystem.Models
+{
+    public class Students
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Guardian Name")]
+        public string? Name { get; set; }
+        [DisplayName("Guardian Name")]
+        public string? GuidanceName { get; set; }
+        [DisplayName("Guardian Phone")]
+        public int GuidanceNo { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
